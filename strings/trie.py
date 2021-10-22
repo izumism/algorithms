6ba879c30f6cc572bb39f14735ac274bfa8d590a
Shioh Izumi <shioh0716iz@gmail.com>
2021-10-22 16:21:38 +0900

    add matching function
    only true or false

--- strings/trie.py
+++ strings/trie.py
@@ -11,23 +11,26 @@ class Node:
         self._symbol = symbol
         self._children = {}
 
-    def has_child_of(self, symbol):
+    def has_child(self, symbol):
         return symbol in self._children
 
     def child_of(self, symbol):
-        if not self.has_child_of(symbol):
+        if not self.has_child(symbol):
             raise RuntimeError(f'no corresponding child: symbol={symbol}')
         return self._children[symbol]
 
     def append(self, node):
         symbol = node.symbol()
-        if self.has_child_of(symbol):
+        if self.has_child(symbol):
             raise RuntimeError(f'given child already exists: symbol={symbol}')
         self._children[symbol] = node
         return self
 
     def symbol(self):
         return self._symbol
+
+    def is_leaf(self):
+        return not bool(self._children)
 
     @staticmethod
     def empty():
@@ -53,9 +56,33 @@ def construct_trie(patterns):
     for pattern in patterns:
         curr = root
         for i, symbol in enumerate(pattern):
-            if curr.has_child_of(symbol):
+            if curr.has_child(symbol):
                 curr = curr.child_of(symbol)
             else:
                 to_be_added = Node.chain_head(pattern[i:])
                 curr = curr.append(to_be_added)
     return root
+
+
+def prefix_match(text, trie):
+    if not text:
+        return text
+    sym_iter = iter(text)
+    curr_sym = next(sym_iter, False)
+    v = trie
+    while curr_sym:
+        if v.is_leaf():
+            return True
+        elif v.has_child(curr_sym):
+            v = v.child_of(curr_sym)
+            curr_sym = next(sym_iter, False)
+        else:
+            return False
+    return False
+
+
+def trie_matching(text, trie):
+    result = False
+    for i in range(len(text)):
+        result = prefix_match(text[i:], trie)
+    return result
