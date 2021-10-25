6a007ff28954b1057abd8acfeed69a0a4e371c97
Shioh Izumi <shioh0716iz@gmail.com>
2021-10-25 16:34:15 +0900

    implement burrows wheeler transform

---
+++ strings/burrows_wheeler.py
@@ -0,0 +1,104 @@
+def num_char(num, char):
+    return str(num) + char
+
+
+class RLItem:
+    def __init__(self):
+        self.length_char = []
+
+    def append(self, length, char):
+        self.length_char.append((length, char))
+
+    def as_encoding(self):
+        return ''.join([
+            str(length) + char for length, char in self.length_char])
+
+    def as_decoding(self):
+        return ''.join([
+            char * length for length, char in self.length_char])
+
+
+def run_length_encoding(text: str) -> RLItem:
+    if not text:
+        return ''
+    head_i = 0
+    head_c = text[0]
+    result = RLItem()
+    curr_i = -1
+    curr_c = ''
+    for i in range(1, len(text)):
+        curr_i = i
+        curr_c = text[i]
+        if head_c == curr_c:
+            continue
+        else:
+            length = curr_i - head_i
+            result.append(length, head_c)
+            head_i = curr_i
+            head_c = curr_c
+    print(f'len: {len(text)}')
+    print(f'head_i: {head_i}, head_c: {head_c}')
+    result.append(len(text)-head_i, curr_c)
+    return result
+
+
+class BWItem:
+    def __init__(self):
+        self.before_after = []
+
+    def append(self, before, after):
+        self.before_after.append((before, after))
+
+    def as_encoding(self):
+        return ''.join([after for before, after in self.before_after])
+
+
+def burrows_wheeler_transform(text):
+    target = text + '$'
+    length = len(target)
+    cycles = [target[i:length] + target[0:i] for i in range(len(target))]
+    result = BWItem()
+    for cycle in sorted(cycles):
+        result.append(cycle[0], cycle[-1])
+    return result
+
+
+def sort_by_first(rows):
+    return sorted(rows, key=lambda row: row[0])
+
+
+# Memory inefficient
+def burrows_wheeler_decoding_bad(bw_code):
+    matrix = [[e] for e in sorted(bw_code)]
+    for _ in range(len(bw_code)-1):
+        matrix = sort_by_first([[bw, *me] for bw, me in zip(bw_code, matrix)])
+    return ''.join(matrix[0])[1:]
+
+
+IndexedChar = (str, int)
+
+
+def decorate_index(bw_code: [str]) -> [IndexedChar]:
+    counter = {}
+    result = []
+    for ch in bw_code:
+        if ch in counter:
+            counter[ch] += 1
+        else:
+            counter[ch] = 0
+        result.append((ch, counter[ch]))
+    return result
+
+
+# Efficient
+def burrows_wheeler_decoding(bw_code):
+    indexed_bw = decorate_index(bw_code)
+    sorted_bw = sorted(indexed_bw)
+    bw2sorted = dict(zip(indexed_bw, sorted_bw))
+    bw_key = sorted_bw[0]
+    result = ''
+    for _ in range(len(bw_code)):
+        sorted_key, i = bw2sorted[bw_key]
+        result += sorted_key
+        bw_key = (sorted_key, i)
+    return result
---
+++ strings/test.py
@@ -0,0 +1,70 @@
+import random
+import string
+import unittest
+
+from burrows_wheeler import (
+    run_length_encoding,
+    burrows_wheeler_transform,
+    burrows_wheeler_decoding_bad,
+    burrows_wheeler_decoding,
+)
+
+
+def random_alphabetic_text(n):
+    return ''.join(random.choices(string.ascii_letters, k=n)).upper()
+
+
+class TestSort(unittest.TestCase):
+    """test case for various implementation of sort algorithm
+    """
+
+    def test_run_length_encoding1(self):
+        input = 'GGGGGGGGGGCCCCCCCCCCCAAAAAAATTTTTTTTTTTTTTTCCCCCG'
+        actual = run_length_encoding(input)
+        expected = '10G11C7A15T5C1G'
+        self.assertEqual(actual.as_encoding(), expected, 'run_length_encoding')
+
+    def test_run_length_encoding2(self):
+        input = 'GGGGGGGGGGCCCCCCCCCCCAAAAAAATTTTTTTTTTTTTTTCCCCC'
+        actual = run_length_encoding(input)
+        expected = '10G11C7A15T5C'
+        self.assertEqual(actual.as_encoding(), expected, 'run_length_encoding')
+
+    def test_run_length_encode_decode(self):
+        input = random_alphabetic_text(10)
+        result = run_length_encoding(input).as_decoding()
+        self.assertEqual(
+            result, input,
+            'run_length encoding/decoding property based testing')
+
+    def test_burrows_wheeler_transform1(self):
+        input = 'panamabananas'
+        actual = burrows_wheeler_transform(input)
+        expected = 'smnpbnnaaaaa$a'
+        self.assertEqual(
+            actual.as_encoding(), expected,
+            'burrows_wheeler_transform')
+
+    def test_burrows_wheeler_transform2(self):
+        input = 'AGACATA'
+        actual = burrows_wheeler_transform(input)
+        expected = 'ATG$CAAA'
+        self.assertEqual(
+            actual.as_encoding(), expected,
+            'burrows_wheeler_transform')
+
+    def test_burrows_wheeler_decoding_bad(self):
+        input = 'annb$aa'
+        actual = burrows_wheeler_decoding_bad(input)
+        expected = 'banana'
+        self.assertEqual(actual, expected, 'burrows_wheeler_decoding_bad')
+    
+    def test_burrows_wheeler_decoding(self):
+        input = 'smnpbnnaaaaa$a'
+        actual = burrows_wheeler_decoding(input)
+        expected = 'panamabananas$'
+        self.assertEqual(actual, expected, 'burrows_wheeler_decoding')
+
+
+if __name__ == "__main__":
+    unittest.main()
