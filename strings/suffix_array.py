2fa1233cccb538a13cc093127f0f97ac5df94328
Shioh Izumi <shioh0716iz@gmail.com>
2021-11-01 11:35:06 +0900

    add suffix array construction (better one)

--- strings/suffix_array.py
+++ strings/suffix_array.py
@@ -18,3 +18,34 @@ def suffix_array(string) -> [int]:
     return [
         pos for (suff, pos) in sorted(suff_pos.items(), key=lambda x: x[0])
     ]
+
+
+def get_cyclic_order_index(string, length) -> [int]:
+    result = {}
+    source_len = len(string)
+    for pos in range(source_len):
+        start = pos
+        end = start + length
+        if end >= source_len:
+            cyclic_substr = string[start:] + string[:end % source_len]
+            result[pos] = cyclic_substr
+        else:
+            cyclic_substr = string[start:end]
+            result[pos] = cyclic_substr
+    return [
+        pos for (pos, substr) in sorted(result.items(), key=lambda x: x[1])
+    ]
+
+
+def construct_suffix_array(string) -> [int]:
+    curr_len = 1
+    string_len = len(string)
+    prev_arr = get_cyclic_order_index(string, curr_len)
+    while True:
+        # assert curr_len <= string_len, 'implementation error'
+        curr_len *= 2
+        curr_arr = get_cyclic_order_index(string, curr_len)
+        if curr_arr == prev_arr:
+            return prev_arr
+        prev_arr = curr_arr
+    assert False, 'this function returns in while loop'
--- strings/suffix_array_test.py
+++ strings/suffix_array_test.py
@@ -3,6 +3,8 @@ import unittest
 from suffix_array import (
     suffixes_lexicographic_order,
     suffix_array,
+    get_cyclic_order_index,
+    construct_suffix_array,
 )
 
 
@@ -19,6 +21,27 @@ class TestSuffixArray(unittest.TestCase):
     def test_suffix_array(self):
         string = 'ababaa$'
         actual = suffix_array(string)
+        expected = [6, 5, 4, 2, 0, 3, 1]
+        self.assertListEqual(actual, expected, 'suffix array')
+
+    def test_cyclic_order_index(self):
+        string = 'ababaa$'
+        expected = [6, 0, 2, 4, 5, 1, 3]
+        actual = get_cyclic_order_index(string, 1)
+        self.assertListEqual(actual, expected, 'cyclic_order, 1')
+        expected = [6, 5, 4, 0, 2, 1, 3]
+        actual = get_cyclic_order_index(string, 2)
+        self.assertListEqual(actual, expected, 'cyclic_order, 2')
+        expected = [6, 5, 4, 2, 0, 3, 1]
+        actual = get_cyclic_order_index(string, 4)
+        self.assertListEqual(actual, expected, 'cyclic_order, 4')
+        expected = [6, 5, 4, 2, 0, 3, 1]
+        actual = get_cyclic_order_index(string, 8)
+        self.assertListEqual(actual, expected, 'cyclic_order, 4')
+    
+    def test_construct_suffix_array(self):
+        string = 'ababaa$'
+        actual = construct_suffix_array(string)
         expected = [6, 5, 4, 2, 0, 3, 1]
         self.assertListEqual(actual, expected, 'suffix array')
 
