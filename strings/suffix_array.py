842d7580bd342e7d2e943673eec0a2448ffb4820
Shioh Izumi <shioh0716iz@gmail.com>
2021-11-01 10:57:49 +0900

    modify implementation for more memory efficiently

--- strings/suffix_array.py
+++ strings/suffix_array.py
@@ -19,10 +19,7 @@ def sublist_to_dollar(string: str) -> str:
 def suffix_array(string) -> [int]:
     suff_pos = {}
     for pos in range(len(string)):
-        cycle = string[pos:] + string[:pos]
-        # Need only sublist to '$' for ordering
-        truncated = sublist_to_dollar(cycle)
-        suff_pos[truncated] = pos
+        suff_pos[string[pos:]] = pos
     return [
         pos for (suff, pos) in sorted(suff_pos.items(), key=lambda x: x[0])
     ]
