ffd614fcd2ddd719fa1d343b1a2be0d499d563c5
Shioh Izumi <shioh0716iz@gmail.com>
2021-11-02 10:23:04 +0900

    replace with general alphabet-orderId dictionary data structure

--- strings/sedgewick/key_indexed_counting.py
+++ strings/sedgewick/key_indexed_counting.py
@@ -1,22 +1,16 @@
-ALPHA2INDEX = {chr(ord('a')+i): i for i in range(26)}
-INDEX2ALPHA = {i: ch for ch, i in ALPHA2INDEX.items()}
-
-
-def create_alpha_2_index(charset):
-    return {char: i for i, char in enumerate(sorted(set(charset)))}
+from alphabets import ALPH2INDEX
 
 
 def key_indexed_counting(string):
-    alpha2index = create_alpha_2_index(string)
-    count_sz = len(alpha2index)
+    count_sz = len(ALPH2INDEX)
     count = [0] * (count_sz + 1)
     for ch in string:
-        count[alpha2index[ch]+1] += 1
+        count[ALPH2INDEX[ch]+1] += 1
     for i in range(count_sz-1):
         count[i+1] += count[i]
     str_len = len(string)
     result = [0] * str_len
     for ch in string:
-        result[count[alpha2index[ch]]] = ch
-        count[alpha2index[ch]] += 1
+        result[count[ALPH2INDEX[ch]]] = ch
+        count[ALPH2INDEX[ch]] += 1
     return result
